130fb1ee85fc2280e0507a7affa8de5bc9dbd65a
Mohamed Moustafa <92525570+Dev-MohamedMoustafa@users.noreply.github.com>
2021-10-28 10:36:28 +0200

    Add files via upload
    initial commit

---
+++ AbstractFactory.js
@@ -0,0 +1,35 @@
+/*
+ * this code was written by Mohamed Moustafa
+ * As a preview of his knowledge in Design Patterns
+ * 
+ */
+
+function Employee(name) {
+    this.name = name;
+
+    this.say = function () {
+        console.log("I am employee " + name);
+    };
+}
+
+function EmployeeFactory() {
+
+    this.create = function (name) {
+        return new Employee(name);
+    };
+}
+
+function Vendor(name) {
+    this.name = name;
+
+    this.say = function () {
+        console.log("I am vendor " + name);
+    };
+}
+
+function VendorFactory() {
+
+    this.create = function (name) {
+        return new Vendor(name);
+    };
+}
